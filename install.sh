2f790f3ab95c500e48f501ba139f061c660c0467
Jackafur <jackafursrl@gmail.com>
2025-05-27 19:17:19 -0400

    Updated install.sh to be portable for other users

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-MODS_DIR="/home/sm64sock/sm64coopdx/mods/honorable-mentions"
+# Default to $HOME/sm64coopdx/mods if SM64COOPDX_MODS_DIR is not set
+MODS_DIR="${SM64COOPDX_MODS_DIR:-$HOME/sm64coopdx/mods}/honorable-mentions"
+
+# Create the mods directory if it doesn't exist
 mkdir -p "$MODS_DIR"
-cp -r honorable-mentions/* "$MODS_DIR"
-echo "Installed to $MODS_DIR"
+
+# Copy mod files to the mods directory
+if [ -d "honorable-mentions" ]; then
+    cp -r honorable-mentions/* "$MODS_DIR"
+    echo "Installed mod to $MODS_DIR"
+else
+    echo "Error: 'honorable-mentions' folder not found in the current directory"
+    exit 1
+fi
